bf42b253d7099db36fd607d707072ec525a1bcd9
ManApart <raustinkucera@gmail.com>
2019-06-08 00:44:05 -0400

    module not used, app coming up

--- app/src/main/java/rak/org/questcommandmobile/MainActivity.kt
+++ app/src/main/java/rak/org/questcommandmobile/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-import core.commands.CommandParser
 
 import kotlinx.android.synthetic.main.activity_main.*
 
